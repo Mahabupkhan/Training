51bdbf410ba9e78f11d4342875c5cc7a428c7a2a
Mahabupkhan <98726833+Mahabupkhan@users.noreply.github.com>
2022-09-09 11:06:12 +0530

    Update Assign2.java

--- Assign2.java
+++ Assign2.java
@@ -6,7 +6,7 @@ public class Assign2 {
 
 		int x = 100, y = 200;
 
-		System.out.println("Before Swap");
+		System.out.println("-----Before Swap-----");
 		System.out.println("x = " + x);
 		System.out.println("y = " + y);
 
@@ -14,7 +14,7 @@ public class Assign2 {
 		x = y;
 		y = temp;
 
-		System.out.println("After swap");
+		System.out.println("-----After swap-----");
 		System.out.println("x = " + x);
 		System.out.println("y = " + y);
 	}
